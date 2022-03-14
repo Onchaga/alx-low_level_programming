1bc12fb3d3c2d51f5aa8ed3fe8df5b76855784f0
Onchaga <dianamokei@gmail.com>
2022-03-14 15:48:10 -0700

    Task 9 commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,22 +6,20 @@
  */
 int main(void)
 {
-	int number;
+int number;
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+if (number == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
 
-	for (number = 48; number <= 57; number++)
-	{
-		putchar(number);
-	
-		if (number == 57)
-		{
-			break;
-		}
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-
-	return (0);
+return (0);
 
 }
 
