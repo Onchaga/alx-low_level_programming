2a385f17a49a3e1a2b87b7f9d24c21a9fbba8e16
Onchaga <dianamokei@gmail.com>
2022-03-14 14:42:18 -0700

    Task 6 commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	for (i = 48; i <= 57; i++)
-	{
+for (i = 48; i <= 57; i++)
+{	
+putchar (i);
+}
+putchar ('\n');
 	
-		putchar (i);
-	}
-	putchar ('\n');
-	
-	return (0);
+return (0);
 }
 
 
