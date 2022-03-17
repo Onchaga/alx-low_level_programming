07fd5bd979166b9c50c76fcca235680f361bf07f
Onchaga <dianamokei@gmail.com>
2022-03-16 17:20:34 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_abs - absolute value
+ *
+ *@n: the int to print
+ *Return: return 0 always
+ */
+int _abs(int)
+{
+if (n < 0)
+{
+return (n);
+}
+else if (n == 0)
+{
+return (0);
+}
+else (n > 0)
+{
+return (n);
+}
+}
