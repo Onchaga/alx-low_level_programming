269e187cd3c015782ed18161d3f784879b4f0464
Onchaga <dianamokei@gmail.com>
2022-03-16 23:32:03 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,7 @@
  */
 int print_last_digit(int n)
 {
+int last_digit;
 if (n < 0)
 {
 last_digit = (-1 * (n % 10));
@@ -18,7 +19,8 @@ else
 last_digit = (n % 10);
 _putchar (last_digit + '0');
 return (last_digit);
-}}
+}
+}
 
 
 
