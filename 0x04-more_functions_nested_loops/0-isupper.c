5aa7c78bc265867574837cf0988950cd11f3e1c3
Onchaga <dianamokei@gmail.com>
2022-03-20 08:18:28 -0700

    int _isupper(int c) commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: Print int
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
