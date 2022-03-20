43a4f58e02cd937128c15bd9cb4107b64655f5b6
Onchaga <dianamokei@gmail.com>
2022-03-20 09:17:56 -0700

    3-print_numbers.c commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return 0: always 0
+ */
+void print_numbers(void)
+{
+int i:
+i = 0;
+while (i <= 9; i++)
+{
+_putchar (i);
+}
+_putchar('\n');
+}
