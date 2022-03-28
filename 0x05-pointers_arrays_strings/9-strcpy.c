d0fc14fa0abb8bdf4cd88b89514f78527d6da481
Onchaga <dianamokei@gmail.com>
2022-03-28 06:14:34 -0700

    9-strcpy.c commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcpy - copies the string pointed by src
+ *@dest: a pointer to char that will be charged
+ *@src - a pointer to a char that will be changed
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
