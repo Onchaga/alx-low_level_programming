292cbc39db8fc6a1cbce509130fc499ee1e25b05
Onchaga <dianamokei@gmail.com>
2022-03-28 06:27:45 -0700

    0-reset_to_98.c commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,9 @@
 /**
 *reset_to_98 - updates an integer value to 98
 *@n:a pointer to an integer that will be updated
-*Return: 0 always
+*Return: void that means is correct
 */
 void reset_to_98(int *n)
 {
 *n = 98;
 }
- 
